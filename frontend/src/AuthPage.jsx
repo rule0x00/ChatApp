8542b043a4019f7074203bb78e9af543ebfc7a06
rule0x00 <aakashbhardwaj0647@gmail.com>
2025-04-19 19:38:42 +0530

    welcome

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -16,7 +16,7 @@ const AuthPage = (props) => {
   return (
     <div className="background">
       <form onSubmit={onSubmit} className="form-card">
-        <div className="form-title">Welcome motherfuckers 👋</div>
+        <div className="form-title">Welcome! 👋</div>
 
         <div className="form-subtitle">Set a username to get started</div>
 
